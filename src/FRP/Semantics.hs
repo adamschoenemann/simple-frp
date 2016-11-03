817f740fab4992c60ba6b4c76a6faa071b7c53be
Adam Schønemann <adamschoenemann@gmail.com>
2016-11-03 23:57:15 +0100

    more small work but not sure this is the right monad

--- src/FRP/Semantics.hs
+++ src/FRP/Semantics.hs
@@ -18,10 +18,20 @@ evalStep e = case e of
   TmSnd trm -> do
     VTup x y <- evalStep trm
     return y
---   TmTup trm1 trm2
---   TmInl trm
---   TmInr trm
---   TmCase trm (nml, trml) (nmr, trmr)
+  TmTup trm1 trm2 -> VTup <$> evalStep trm1 <*> evalStep trm2
+  TmInl trm -> VInl <$> evalStep trm
+  TmInr trm -> VInr <$> evalStep trm
+  TmCase trm (nml, trml) (nmr, trmr) -> do
+    res <- evalStep trm
+    case res of
+      VInl vl -> evalStep (subst vl nml trml)
+      VInr vr -> evalStep (subst vr nmr trmr)
+      _       -> undefined
+
+subst :: Value -> Name -> Term -> Term
+subst = undefined
+--subst
+
 --   TmLam nm trm
 --   TmVar nm
 --   TmApp trm1 trm2
